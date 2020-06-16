b3f77d89eba14d04ec0bb2c369a07fdc08d6432b
twb2511 <twb2511@rit.edu>
2020-06-16 09:51:03 -0400

    Initial project commit

---
+++ Humans vs Zombies/Assets/Scripts/Human.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : Agent
+{
+    // Human specific variables
+    public Material humanFuturePos;                     // To implement in p-04
+    public GameObject predator;                         // The zombie closest zombie
+    public GameObject[] predators = new GameObject[0];  // Array of near by zombies
+	
+	// Update is called once per frame ***************************************************************
+	void Update ()
+    {
+        // Stay in bounds
+        Boundaries();
+
+        // Obtain a reference to array of all humans
+        GameObject[] allHumans = sceneManager.GetComponent<Management>().humans;
+
+        
+
+        // If the human is not wandering
+        if (!wandering)
+        {
+            // Flee closest zombie
+            //Flee(predator);
+
+            // Evade closest zombies
+            Evade(predator);
+
+            // Apply forces and move
+            Move();
+        }
+
+        // While the zombie is wandering
+        else
+        {
+            // Wander (P-04)
+            Wander();
+            Move();
+        }
+
+        // Separation between like objects
+        Separate(allHumans);
+
+        // Avoid Obsticles
+        Avoid();
+
+        // Calculate new future position (1/4 sec out)
+        CalcFuturePos();
+
+    }
+
+    // Additional debug lines *************************************************************************
+    protected override void OnRenderObject()
+    {
+        if (sceneManager != null && sceneManager.GetComponent<Management>().debugMode)
+        {
+            // Draw lines from base class
+            base.OnRenderObject();
+
+            // Draw line for humans future position
+            humanFuturePos.SetPass(0);
+            GL.Begin(GL.LINES);
+            GL.Vertex(transform.position);
+            GL.Vertex(futurePosition);
+            GL.End();
+        }
+    }
+
+    // Calculates flee force ****************************************************************************************
+    public void Flee(Vector3 predatorPosition)
+    {
+        // Calculate desired velocity
+        Vector3 desiredVelocity = agentPosition - predatorPosition;
+
+        // Scale desired velocity
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+
+        // Rotate agent
+        transform.forward = desiredVelocity * Time.deltaTime;
+
+        // Calculate the force 
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        // Apply steering force
+        ApplyForce(steeringForce);
+    }
+
+    // Overloaded flee that takes game object ***********************************************************************
+    public void Flee(GameObject closestZombie)
+    {
+        // Call seek passing target position
+        Flee(closestZombie.transform.position);
+    }
+
+    // Calculates evasion force ****************************************************************************************
+    public void Evade(Vector3 predatorFuturePosition)
+    {
+        // Calculate desired velocity
+        Vector3 desiredVelocity = agentPosition - predatorFuturePosition;
+
+        // Scale desired velocity
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+
+        // Rotate agent
+        transform.forward = desiredVelocity * Time.deltaTime;
+
+        // Calculate the force 
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        // Apply steering force
+        ApplyForce(steeringForce);
+    }
+
+    // Overloaded evasion that takes array of game objects ***********************************************************************
+    public void Evade(GameObject closestZombie)
+    {
+        // Call evade passing targets future position
+        Evade(closestZombie.GetComponent<Zombie>().futurePosition);
+    }
+
+}
---
+++ Humans vs Zombies/Assets/Scripts/Zombie.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : Agent
+{
+    // Zombie specific variables
+    public Material zombieToHuman;      // Distance to nearest human
+    public Material zombieFuturePos;    // To implement in p-04
+    public GameObject target;           // Closest human
+	
+	// Update is called once per frame ***********************************************************
+	void Update ()
+    {
+        // Stay in bounds
+        Boundaries();
+
+        // Obtain a reference to array of all zombies
+        GameObject[] allZombies = sceneManager.GetComponent<Management>().zombies;
+
+        
+
+        // If the zombie is not wandering
+        if (!wandering)
+        {
+            // Seek closest human
+            //Seek(target);
+
+            // Pursue closest human
+            Pursue(target);
+
+            // Apply forces and move
+            Move();
+        }
+        
+        // While the zombie is wandering
+        else
+        {
+            // Wander (P-04)
+            Wander();
+            Move();
+        }
+
+        // Separation between zombies
+        Separate(allZombies);
+
+        // Avoid Obsticles
+        Avoid();
+
+        // Calculate agents new future position (1/4 second out)
+        CalcFuturePos();
+	}
+
+    // Additional debug lines ****************************************************************
+    protected override void OnRenderObject()
+    {
+        // If the scene is in debug mode
+        if (sceneManager != null && sceneManager.GetComponent<Management>().debugMode)
+        {
+            // Draw lines from base class
+            base.OnRenderObject();
+
+            // If a target has been acquired
+            if (target != null)
+            {
+                // Draw line from zombie to closest human
+                zombieToHuman.SetPass(0);
+                GL.Begin(GL.LINES);
+                GL.Vertex(transform.position);
+                GL.Vertex(target.transform.position);
+                GL.End();
+            }
+
+            //Draw line for zombie's future position
+            zombieFuturePos.SetPass(0);
+            GL.Begin(GL.LINES);
+            GL.Vertex(transform.position);
+            GL.Vertex(futurePosition);
+            GL.End();
+        }
+    }
+
+    // Calculates seeking force ****************************************************************************************
+    public void Seek(Vector3 targetPosition)
+    {
+        // Calculate desired velocity
+        Vector3 desiredVelocity = targetPosition - agentPosition;
+
+        // Scale desired velocity
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+
+        // Rotate agent
+        transform.forward = desiredVelocity * Time.deltaTime;
+
+        // Calculate the force 
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        // Apply steering force
+        ApplyForce(steeringForce);
+    }
+
+    // Overloaded seek that takes game object **************************************************************************
+    public void Seek(GameObject closestHuman)
+    {
+        // Call seek passing target position
+        Seek(closestHuman.transform.position);
+    }
+
+    // Calculates pursue force ***************************************************************************************
+    public void Pursue(Vector3 targetFuturePosition)
+    {
+        // Calculate desired velocity
+        Vector3 desiredVelocity = targetFuturePosition - agentPosition;
+
+        // Scale desired velocity
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+
+        // Rotate agent
+        transform.forward = desiredVelocity * Time.deltaTime;
+
+        // Calculate the force 
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        // Apply steering force
+        ApplyForce(steeringForce);
+    }
+
+    // Overloaded pursue that takes game object **************************************************************************
+    public void Pursue(GameObject closestHuman)
+    {
+        // Call pursue passing target position
+        Seek(closestHuman.GetComponent<Human>().futurePosition);
+    }
+}
